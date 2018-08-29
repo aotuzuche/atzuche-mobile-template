0fbbfd505b586a1c65e7b94565f415264dbbfd74
shaodahong <337097324@qq.com>
2018-08-29 17:19:35 +0800

    修改一些模板语法

--- appConfig.js
+++ appConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   // 一般情况下，它的值是 /${prodPathPrefix}/${prodPath}
-  basename: process.env.NODE_ENV === 'production' ? '/m/<%= prodPath %>' : '',
+  basename: process.env.NODE_ENV === 'production' ? '/<%= projectType %>/<%= prodPath %>' : '',
 
   // 打包出口目录的前缀，注意：不需要以/开头
-  prodPathPrefix: 'm',
+  prodPathPrefix: '<%= projectType %>',
 
   // 打包的出口目录(默认dist目录)
   prodPath: '<%= prodPath %>'
