a753f78c6aba2c891a1c5cb1ffe959d691660499
shaodahong <337097324@qq.com>
2018-09-28 10:37:54 +0800

    去掉 build 该用 autos 公用脚手架构建

--- build/auto-prod-filter-loader/lib/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (source) {
-  if (process.env.PACKAGE === 'test') {
-    return source
-  } else {
-    const match = /\/\/\s*?>>>(\s|.)*?\/\/\s*?<<</g
-    return source.replace(match, '')
-  }
-}
